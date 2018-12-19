8d41698844c1ef9b340cb75f1f3309bc05d32513
ShiraNegbi <Shiran96@gmail.com>
2018-12-19 18:41:38 +0200

    maps

--- Maps.cpp
+++ Maps.cpp
@@ -2,11 +2,12 @@
 // Created by shira on 12/19/18.
 //
 
-#include "VarBindMap.h"
+#include "Maps.h"
 #include <map>
 using namespace std;
 
-class VarBindMap
+class Maps
 {
     map<string, string> bindMap;
+    map<string, double> valMap;
 };

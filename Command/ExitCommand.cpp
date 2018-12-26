78c3b6ba637e70f878321cd654aa5028680228e4
ShiraNegbi <Shiran96@gmail.com>
2018-12-26 18:54:22 +0200

    ***Backup version before changing the  implementation of Parser*** Implementing ConditionCommand, WhileCommand, IfCommand, handling conditions in Parser, Expression, BinaryExpression.

---
+++ Command/ExitCommand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shira on 12/26/18.
+//
+
+#include "ExitCommand.h"
---
+++ Command/ExitCommand.h
@@ -0,0 +1,8 @@
+//
+// Created by shira on 12/26/18.
+//
+
+#ifndef MILESTONE_EXITCOMMAND_H
+#define MILESTONE_EXITCOMMAND_H
+
+#endif //MILESTONE_EXITCOMMAND_H

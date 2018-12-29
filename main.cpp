109833e188b02cfbe0ef9f7e7ca0219722210c88
shay <shayslonim@gmail.com>
2018-12-29 18:15:09 +0200

    Fixed the main method, it now has (at least the start of) the implementation.

--- main.cpp
+++ main.cpp
@@ -11,27 +11,22 @@ static void printVector(vector<string> array);
 
 int main(int argc, char* argv[]) {
 
-//    ifstream inFile;
-//    string buffer;
-//    inFile.open(argv[0]);
-//    if (!inFile) {
-//        cerr << "Unable to open file datafile.txt";
-//        exit(1);   // call system to stop
-//    }
-//    vector<string> lines;
-//    while (getline(inFile, buffer)) {
-//        lines.push_back(buffer);
-//    }
-//    vector<string>::iterator iter = lines.begin();
-//
-//    //todo: call parser with the iterator
-    //OpenDataServerCommand odsc = OpenDataServerCommand(5400, 10, new Maps());o
-    //ConditionCommand* command = new IfCommand(std::vector<std::string>());
-    vector<vector<string>>* iterable = new vector<vector<string>>();
-
-    for (vector<vector<string>>::iterator it = iterable->begin(); it != iterable->end(); ++it) {
-
+    ifstream inFile;
+    string buffer;
+    inFile.open(argv[0]);
+    if (!inFile) {
+        cerr << "Unable to open file datafile.txt";
+        exit(1);   // call system to stop
     }
+    vector<string> lines;
+    while (getline(inFile, buffer)) {
+        lines.push_back(buffer);
+    }
+    vector<string>::iterator iter = lines.begin();
+
+//    //todo: call parser with the iterator
+    //OpenDataServerCommand odsc = OpenDataServerCommand(5400, 10, new Maps());
+    //ConditionCommand* command = new IfCommand(std::vector<std::string>());
 
 }
 

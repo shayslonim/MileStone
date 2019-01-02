14e14101c240f5490fda8f6b8ce731d3b6e1fc0c
shay <shayslonim@gmail.com>
2019-01-02 14:06:21 +0200

    Removed unnecessary comments

--- main.cpp
+++ main.cpp
@@ -1,50 +1,23 @@
 #include <iostream>
-//<<<<<<< HEAD
+
 #include "Expression/ExpressionTest.h"
-//=======
+
 #include "Lexer.h"
 #include "Parser.h"
 #include "Maps.h"
 #include "Command/OpenDataServerCommand.h"
 #include "Command/IfCommand.h"
-//>>>>>>> origin/master
+
 #include <unordered_set>
 #include <fstream>
 
-//
-//
-//int main() {
-//    std::string line = "yay+divide/ love () wow + + ++";
-//    std::cout << "before: " << line << std::endl;
-//    //ExpressionTest::doTest();
-//    std::unordered_set<char> operators = {'+', '-', '/', '*', '(', ')', '='};
-//    int i = 0;
-//    bool inserted = false;
-//    while (i < line.length()) {
-//        if (operators.find(line[i]) != operators.end()) {
-//            if (i < line.length() - 1 && line[i + 1] != ' ') {
-//                line.insert(i + 1, " ");
-//            }
-//            if (i > 0 && line[i - 1] != ' ') {
-//                line.insert(i, " ");
-//                inserted = true;
-//            }
-//
-//<<<<<<< HEAD
-//            if (inserted) {
-//                i++;
-//            }
-//            i++;
-//
-//        }
-//    }
-//    std::cout << "after: " << line << std::endl;
 int main(int argc, char* argv[]) {
     Maps maps = Maps();
     Lexer lexer;
     Parser parser = Parser(&maps);
     ifstream inFile;
     string buffer;
+
     if (argc < 2) {
         throw  "file name not entered";
     }

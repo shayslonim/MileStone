82a1ab733226aebcefd66ba94780746f7191119b
ShiraNegbi <Shiran96@gmail.com>
2019-01-02 13:14:16 +0200

    Implementing the construction of ConnectCommand

--- Command/ConnectCommand.cpp
+++ Command/ConnectCommand.cpp
@@ -6,10 +6,30 @@
 
 ConnectCommand::ConnectCommand(vector<string> line, Maps* maps) {
     this->maps = maps;
-    //todo: constructor
+    InfixHandler infixHandler = InfixHandler();
+    ExpressionFactory factory = ExpressionFactory(maps);
+    int index = 0;
+    int indexSecond;
+    vector<string> first;
+    vector<string> second;
+    if (find(line.begin(), line.end(), ",") != line.end()) {
+        while (line[index] != ",") {
+            index++;
+        }
+        index--;
+        indexSecond = index + 2;
+    }
+    else {
+        if (!(infixHandler.isANumber(line[index]) && infixHandler.isANumber(line[index + 1]))) {
+            index++;
+        }
+        indexSecond = index + 1;
+    }
+    this->portId = factory.getExpressionFromUnorderedLine(line, 1, index)->calculate();
+    this->timesPerSecond = factory.getExpressionFromUnorderedLine(line, indexSecond, line.size())->calculate();
 }
 
 void ConnectCommand::execute() {
     ServerUpdater* serverUpdater = new ServerUpdater(this->hostName, this->portId);
-
+    this->updater = serverUpdater;
 }
--- Command/ConnectCommand.h
+++ Command/ConnectCommand.h
@@ -4,14 +4,18 @@
 
 #ifndef MILESTONE_CONNECT_H
 #define MILESTONE_CONNECT_H
+#include <regex>
 #include "Command.h"
 #include "../Maps.h"
+#include "../InfixHandler.h"
+#include "../ExpressionFactory.h"
 
 class ConnectCommand : public Command
 {
     string hostName;
     int portId;
     Maps* maps;
+    ServerUpdater* updater;
 public:
     ConnectCommand(vector<string> line, Maps* maps);
     void execute(/*vector<string>::iterator*/) override;

301d7ef89f39a6cc02a355583c04fd7b89c18f4e
ShiraNegbi <Shiran96@gmail.com>
2019-01-03 23:23:00 +0200

    Remove quotation marks in BindCommand

--- Command/BindCommand.cpp
+++ Command/BindCommand.cpp
@@ -8,12 +8,25 @@
 // The vector is expected to include strings without spaces, ex: var breaks = bind "/controls/flight/speedbrake"
 void BindCommand::execute(/*vector<string>::iterator iter*/) {
     vector<string>::iterator iter = this->line.begin();
+    string variable;
+    string pathOld;
+    string path = string();
     if (iter[0] == "var") {
         // std::cout << line.size() << std::endl;
-        this->maps->insertBind(iter[FIRST + 1], iter[SECOND + 1]);
+        //this->maps->insertBind(iter[FIRST + 1], iter[SECOND + 1]);
+        variable = iter[FIRST + 1];
+        pathOld = iter[SECOND + 1];
     } else {
-        this->maps->insertBind(iter[FIRST], iter[SECOND]);
+        //this->maps->insertBind(iter[FIRST], iter[SECOND]);
+        variable = iter[FIRST];
+        pathOld = iter[SECOND];
     }
+    for (int i = 0; i < pathOld.length(); i++) {
+        if (pathOld[i] != '"') {
+            path += pathOld[i];
+        }
+    }
+    this->maps->insertBind(variable, path);
 }
 
 BindCommand::BindCommand(vector<string> &line, Maps* maps) {
